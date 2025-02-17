d57b68344495fa3381a78477571616c242a62902
rahulprasad1002 <rahulprasad.dev@gmail.com>
2025-02-18 01:24:36 +0530

    Add Part 7

--- server/app.js
+++ server/app.js
@@ -19,7 +19,19 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ALLOWED_ORIGINS,
+    // origin: ALLOWED_ORIGINS,
+    origin: function (origin, callback) {
+      // allow requests with no origin
+      // (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+      if (ALLOWED_ORIGINS.indexOf(origin) === -1) {
+        var msg =
+          'The CORS policy for this site does not ' +
+          'allow access from the specified Origin.';
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'], // Methods you want to allow
     allowedHeaders: ['Content-Type', 'Authorization'], // Specify headers you want to allow
     credentials: true, // Allow credentials (cookies, authorization headers, etc.)
